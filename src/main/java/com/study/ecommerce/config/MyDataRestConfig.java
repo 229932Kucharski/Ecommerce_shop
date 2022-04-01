aaac0d0062b782a568f9ff7666cd131ac8ecb0b3
Gelo <gelo2424@wp.pl>
2022-04-01 13:27:49 +0200

    exposed ids, findByNameContaining in ProductRepository

--- src/main/java/com/study/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/study/ecommerce/config/MyDataRestConfig.java
@@ -8,8 +8,20 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -26,5 +38,16 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        List<Class> entityCLasses = new ArrayList<>();
+        for (EntityType tempEntityType : entities) {
+            entityCLasses.add(tempEntityType.getJavaType());
+        }
+        Class[] domainTypes = entityCLasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
