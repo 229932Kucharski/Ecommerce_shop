23612f529c214147c520fe5a74e71c7c7139e724
Gelo <gelo2424@wp.pl>
2022-04-12 21:04:26 +0200

    Entities from Purchase

---
+++ src/main/java/com/study/ecommerce/entity/OrderItem.java
@@ -0,0 +1,39 @@
+package com.study.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_item")
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "quantity")
+    private BigDecimal quantity;
+
+    @Column(name = "unit_price")
+    private int unitPrice;
+
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+}
