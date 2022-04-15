80816264ecd67149bc2c5761412a6925c66114bd
Gelo <gelo2424@wp.pl>
2022-04-15 22:47:02 +0200

    removed temp files

--- src/main/java/com/study/ecommerce/service/CheckoutService.java
+++
@@ -1,8 +0,0 @@
-package com.study.ecommerce.service;
-
-import com.study.ecommerce.dto.Purchase;
-import com.study.ecommerce.dto.PurchaseResponse;
-
-public interface CheckoutService {
-    PurchaseResponse placeOrder(Purchase purchase);
-}
