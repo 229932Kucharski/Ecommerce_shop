2ae4172126f225e4d75ba80a8331582271bf38d2
Gelo <gelo2424@wp.pl>
2022-04-24 19:10:53 +0200

    Okta dependency, secured /orders endpoints

---
+++ backend/src/main/java/com/study/ecommerce/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.study.ecommerce.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // protect /api/orders
+        http.authorizeRequests()
+                .antMatchers("/api/orders/**")
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        // add CORS filters
+        http.cors();
+
+        // set response body for 401
+        Okta.configureResourceServer401ResponseBody(http);
+
+    }
+}
